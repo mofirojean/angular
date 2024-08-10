e9d66d7c1f942ffb9e5c4f9e47492e193bda5455
mofirojean <mofirojean@gmail.com>
2024-08-10 15:02:48 +0100

    feat: create geostagging app with angular and ionic

---
+++ phototag/src/app/app.component.ts
@@ -0,0 +1,68 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import {
+  IonApp,
+  IonSplitPane,
+  IonMenu,
+  IonContent,
+  IonList,
+  IonListHeader,
+  IonNote,
+  IonMenuToggle,
+  IonItem,
+  IonIcon,
+  IonLabel,
+  IonRouterOutlet,
+  IonRouterLink,
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import {
+  camera,
+  globe
+} from 'ionicons/icons';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+  standalone: true,
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    CommonModule,
+    IonApp,
+    IonSplitPane,
+    IonMenu,
+    IonContent,
+    IonList,
+    IonListHeader,
+    IonNote,
+    IonMenuToggle,
+    IonItem,
+    IonIcon,
+    IonLabel,
+    IonRouterLink,
+    IonRouterOutlet,
+  ],
+})
+export class AppComponent {
+  public appPages = [
+    {
+      title: 'Take a photo',
+      url: '/capture',
+      icon: 'camera',
+    },
+    {
+      title: 'View gallery',
+      url: '/view',
+      icon: 'globe',
+    },
+  ];
+  constructor() {
+    addIcons({
+      camera,
+      globe
+    });
+  }
+}
