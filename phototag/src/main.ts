ff539defd0415df926517fc92f4e2722d6b6bdc6
mofirojean <mofirojean@gmail.com>
2024-08-10 18:29:02 +0100

    mod: major changes to andriod app

--- phototag/src/main.ts
+++ phototag/src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
@@ -12,3 +13,6 @@ bootstrapApplication(AppComponent, {
     provideRouter(routes, withPreloading(PreloadAllModules)),
   ],
 });
+
+// Call the element loader after the platform has been bootstrapped
+defineCustomElements(window);
