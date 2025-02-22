bb465bb0ab27a9458784a4981a53e7a4580f62ca
mofirojean <mofirojean@gmail.com>
2025-02-22 23:10:27 +0100

    mod: removed styles

--- chatbot/src/app/app.component.ts
+++ chatbot/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styles: ``
 })
 export class AppComponent {
   title = 'chatbot';
