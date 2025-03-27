031dc602a2385e4289ffb33aa3209ddb949612ad
mofirojean <mofirojean@gmail.com>
2025-03-27 22:22:32 +0100

    feat: create youtube service

--- yt-clone/src/app/services/youtube.service.ts
+++ yt-clone/src/app/services/youtube.service.ts
@@ -1,8 +1,8 @@
 import {inject, Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {environment} from '../environments/environment';
+import {environment} from '../../environments/environment';
 import {Observable} from 'rxjs';
-import {Video} from './model/video';
+import {Video} from '../model/video';
 
 @Injectable({
   providedIn: 'root'
